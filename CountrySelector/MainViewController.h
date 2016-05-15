2801c87e5ee3fabac00ee59f76438cff29703aa0
Jenny Galino <jennygalino@gmail.com>
2016-05-15 20:10:50 +0800

    Main view controller: Initial code drop

---
+++ CountrySelector/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  MainViewController.h
+//  CountrySelector
+//
+//  Created by Jenny Galino on 15/5/16.
+//  Copyright © 2016 jmgalino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@property (nonatomic, weak) IBOutlet UITextField* countryTextField;
+
+@end
---
+++ CountrySelector/MainViewController.m
@@ -0,0 +1,17 @@
+//
+//  MainViewController.m
+//  CountrySelector
+//
+//  Created by Jenny Galino on 15/5/16.
+//  Copyright © 2016 jmgalino. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+@end
